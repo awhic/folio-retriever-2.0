431761e3ce20a12a61fb2cd1bb33dace028b6c8d
alex <alexh1199@hotmail.com>
2022-09-08 12:23:33 -0400

    read/write api key file functionality

--- src/main/java/com/awhic/fr/util/ApiUtils.java
+++ src/main/java/com/awhic/fr/util/ApiUtils.java
@@ -4,6 +4,8 @@ import com.awhic.fr.code.exception.InvalidApiTokenException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ApiUtils {
@@ -22,6 +24,25 @@ public class ApiUtils {
             return scanner.next();
         } else {
             throw new InvalidApiTokenException();
+        }
+    }
+
+    public void writeApiKey() {
+        System.out.println("Paste your API key here: ");
+        Scanner inquiry = new Scanner(System.in);
+        String result = inquiry.next();
+        ConsoleUtils consoleUtils = new ConsoleUtils();
+
+        try {
+            FileWriter fileWriter = new FileWriter("src/main/resources/api-key.txt");
+            fileWriter.flush();
+            fileWriter.write(result);
+            fileWriter.close();
+            consoleUtils.sleeper();
+            System.out.println("Success!");
+            System.out.println("");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
